8cfb917f15efe997e3304c5089c1d18b173ad7ec
RubenTSR <ruben.rodrigues@extern.kerberos-compliance.com>
2022-11-15 18:27:44 +0000

    🏷️ update create user dto

--- src/users/dtos/create-user.dto.ts
+++ src/users/dtos/create-user.dto.ts
@@ -1,7 +1,10 @@
-import { User } from '../entities/user.entity';
-
 /**
  *  When creating a user, this should contain all the fields that
  * are required for it.
  */
-export type CreateUserDto = Pick<User, 'email' | 'password' | 'firstName' | 'lastName'>;
+export interface CreateUserDto {
+  readonly email: string;
+  readonly password: string;
+  readonly firstName: string;
+  readonly lastName: string;
+}
