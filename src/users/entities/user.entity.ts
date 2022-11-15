8e834831171f8b7771e4e1b7c4aab9cb5e86cc39
RubenTSR <ruben.rodrigues@extern.kerberos-compliance.com>
2022-11-15 18:28:24 +0000

    🏷️ update user entity

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,4 +1,5 @@
 import { v4 } from 'uuid';
+import { UserDto } from '../dtos/user.dto';
 
 /**
  * User Entity
@@ -7,7 +8,7 @@ import { v4 } from 'uuid';
 export class User {
   readonly id: string;
   email: string;
-  password: string;
+  passwordHash: string;
   firstName: string;
   lastName: string;
 
@@ -17,5 +18,14 @@ export class User {
 
     // If an id is received, that's a user update so we must keep it.
     this.id = id ? id : v4();
+  }
+
+  static toDto(user: User): UserDto {
+    return {
+      id: user.id,
+      email: user.email,
+      firstName: user.firstName,
+      lastName: user.lastName,
+    };
   }
 }
